b3369b3732e4f59705430fc1081b41e332efecdf
Timothy Madegwa <timothymadegwa@gmail.com>
2022-03-30 11:02:47 +0300

    connected templates to be rendered by Django

--- main/views.py
+++ main/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+# @login_required(login_url='login')
+def home(request):
+    return render(request, 'main/home.html')
+
+def profile(request):
+    return render(request, 'main/profile.html')
+
+def login(request):
+    return render(request, 'main/login.html')
+
+def register(request):
+    return render(request, 'main/register.html')
